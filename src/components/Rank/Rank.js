71e22f7c949d976d819f094e10162cd5a67efdcd
Jamey LaMar <102421586+SuperJamey@users.noreply.github.com>
2023-02-11 18:32:42 -0600

    add react-tsparticles for display

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Rank = () => {
+  return (
+    <div>
+      <div className='white f3'>
+        { 'Your Rank is... ' }
+      </div>
+      <div className='f1 light-blue'>
+        { '#5' }
+      </div>
+
+    </div>
+  );
+}
+
+export default Rank;
